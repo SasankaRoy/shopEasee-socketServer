f5725d2268faaa38b381af6ce4a52865c5b6ab67
SasankaRoy <104346053+SasankaRoy@users.noreply.github.com>
2024-02-24 18:31:35 +0530

    new order,cancel,update order code

--- index.js
+++ index.js
@@ -23,25 +23,64 @@ const io = new Server(expressServer, {
     ],
   },
 });
-
+// active user list store...
 let connectUserList = [];
 
+
+// connection  event....
 io.on("connection", (socket) => {
   console.log("the user is connect");
 
   const newUserId = socket.handshake.query.userId;
+  const userRole = socket.handshake.query.role;
+
 
   if (!connectUserList.includes(newUserId)) {
-    connectUserList.push({ userId: newUserId, socketId: socket.id });
+    connectUserList.push({ userId: newUserId, socketId: socket.id, userRole });
   }
 
-  let singleUserId;
 
+
+
+  let singleUserId, adminUser;
+
+  // new order or getting order's.........
+  socket.on('NEW__ORDER',async(newOrder)=>{
+    const {oder, productIds, userId, subTotal} = newOrder;    
+
+    const newOder = new Order({
+      userId,
+      userName: oder.fullName,
+      email: oder.email,
+      address: oder.fullAddress,
+      phoneNumber: oder.phoneNumber,
+      alternativePhoneNumber: oder.alternatePhoneNumber,
+      country: oder.country,
+      paymentMethod: oder.payMethod,
+      totalPrice: subTotal,
+      itemList: productIds,
+    })
+    const savedOrder =await newOder.save();
+
+    adminUser = connectUserList.find(admin => admin.userRole === 'admin');
+
+    console.log(adminUser,'adminUser line no 67')
+
+    io.to(adminUser.socketId).emit('NEW__ORDERLIST',{savedOrder})
+
+    socket.emit('ORDER__PLACED',{success:'your order have been placed'});  // sending the response back to the client to notify...
+
+  })
+
+
+// when the admin changes the status of a  single user..
   socket.on("CHANGE__STATUS", ({ productId, userId, oderStatus }) => {
 
     // finding the user from the connected user array...
 
     singleUserId = connectUserList.find((user) => user.userId === userId);
+
+    
 
     if (singleUserId) {
       io.to(singleUserId.socketId).emit("STATUS__CHANGED", { oderStatus });
@@ -50,27 +89,32 @@ io.on("connection", (socket) => {
     }
   });
 
+
+  // cancel order event...
   socket.on('CANCEL__ORDER', async ({ userId, orderId }) => {
     try {
-      const findOrder = await Order.findOneAndUpdate({ _id: orderId }, { "status": "cancel" }, {  returnOriginal:false }).exec();
-      if(findOrder){
-        const newOrderList = await Order.find({userId});
+      const findOrder = await Order.findOneAndUpdate({ _id: orderId }, { "status": "cancel" }, { returnOriginal: false }).exec();
+      if (findOrder) {
+        const newOrderList = await Order.find({ userId });
 
-        singleUserId = connectUserList.find((user)=> user.userId === userId);
+        singleUserId = connectUserList.find((user) => user.userId === userId); // for the user or the client....
+        adminUser = connectUserList.find(admin => admin.userRole === 'admin');        
 
-        if(singleUserId){          
-          io.to(singleUserId.socketId).emit('UPDATED__ORDERLIST',{orders:newOrderList})
+        if (singleUserId) {
+          io.to(singleUserId.socketId).emit('UPDATED__ORDERLIST', { orders: newOrderList })
         }
-         
-      }
 
-      
+        if(adminUser){
+          io.to(adminUser.socketId).emit('ORDERLIST__UPDATE',{orders : newOrderList});
+        }
+      }
 
     } catch (error) {
       console.log("error", error);
     }
   })
 
+  // disconnecting event....
   socket.on("disconnect", () => {
     const disconnectUser = connectUserList.findIndex(
       (user) => user.socketId === socket.id
@@ -81,8 +125,3 @@ io.on("connection", (socket) => {
     }
   });
 });
-
-
-
-
-
